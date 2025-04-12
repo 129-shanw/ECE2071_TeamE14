2e4ca5922eff3f603783ed524b282c864b05efc9
louispham467 <61956270+louispham467@users.noreply.github.com>
2025-04-13 03:24:24 +1000

    head v1

---
+++ temp/ECE2071/STM32Code.py
@@ -0,0 +1,40 @@
+import serial
+import serial.tools.list_ports
+
+BAUD_RATE = 115200
+NUM_OF_DATA_POINTS = 9
+
+#Serial port
+devices = serial.tools.list_ports.comports()
+for device in devices:
+    if (device.description == "STM32 STLink"):
+        STM_device = device.device
+
+ser = serial.Serial(STM_device, BAUD_RATE)  
+
+# # Send message
+data_send = "E"
+# data_send = input("Enter data: ")
+# ser.write(data_send.encode())  
+# print(f"Sent request: {data_send}")
+
+# Receive message 
+# data_receive = ser.readline().decode()
+# print(f"Received: {data_receive}")
+
+# Receive set number of data points and write into file
+file = "data.txt"
+f = open("data.txt", "w")
+for i  in range(NUM_OF_DATA_POINTS):
+    ser.write(data_send.encode()) 
+    data_receive = ser.readline().decode()
+    f.write(data_receive)
+
+# f = open("data.txt", "w")
+# ser.write(data_send.encode()) 
+# data_receive = ser.readline().decode()
+# f.write(data_receive)
+
+# Close
+f.close()
+ser.close()
